3369e4da15cec42455f3ef6cebd1854245d592c6
manu2127 <manojkb012@gmail.com>
2024-07-17 18:39:04 +0530

    running

---
+++ Ketnipz.java
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MOBILE WORLD</title>
+
+</head>
+<body>
+    <h1>Apple</h1>
+    <p>There is one iPhone feature that even the staunchest Android users love; you guessed it, it's the iPhone's phenomenal camera. Apple has engineered the camera on the iPhone to perfection, and there is hardly a smartphone around with a better camera than the latest iPhone.</p>
+   
+    <h2>Samsung Galaxy S23 ultra</h2>
+    <p>The upgraded Snapdragon® 8 Gen 2 Mobile Platform for Galaxy on the S23 Ultra makes for an immersive gaming experience with an upgraded 40% faster GPU and NPU¹. Real-time ray tracing gives gamers the ultimate digital realism on a mobile device!</p>
+    
+    <h3>One Plus</h3>
+    <p>Ultimately, one of the most compelling reasons to buy a OnePlus phone is its battery life and charging speed.</p>
+
+    <h4>Oppo A3 Pro</h4>
+    <p>The company says that the new Oppo A3 Pro is damage-proof as it has an "Armour Body" and features a new internal structure and a suite of drop-resistant materials like Blue Glass double-tempered glass for the screen. </p>
+
+    <h5>Real me GT5</h5>
+    <p>realme became one of the first smartphone manufacturers equipped with Qualcomm Snapdragon 865 5G chips. realme launched its first 5G flagship smartphone, realme X50 Pro 5G.</p>
+
+    <h6>Vivo Y58 5G</h6>
+    <p>It has a super-clear 13MP main rear camera, and a 2MP secondary rear camera which captures depth-of-field information, achieving DSLR-rivalling bokeh shots. </p>
+</body>
+</html>
